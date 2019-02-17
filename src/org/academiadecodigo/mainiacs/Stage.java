246776a080a10caf191341dfdc7adfa090282320
tnbahia <tnbahia@gmail.com>
2019-02-17 21:02:16 +0000

    Stage work

--- src/org/academiadecodigo/mainiacs/Stage.java
+++ src/org/academiadecodigo/mainiacs/Stage.java
@@ -4,12 +4,21 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Stage {
     //private Music music;
-    private Picture background;
+    private StageType stageType;
     private Player player;
     private Counter counter;
 
-    public void play() {
-        
+    public Stage(Player player, StageType stageType) {
+        this.player = player;
+        this.stageType = stageType;
+        counter = new Counter();
     }
 
+    public void start() {
+        drawStage();
+    }
+
+    private void drawStage(){
+        stageType.getBackground().draw();
+    }
 }
