a9a206a7c4f735f7d98995d5b015160c7e4bc12b
tnbahia <tnbahia@gmail.com>
2019-02-21 20:28:03 +0000

    altered loop method

--- src/org/academiadecodigo/mainiacs/Game.java
+++ src/org/academiadecodigo/mainiacs/Game.java
@@ -10,7 +10,7 @@ public class Game {
     public static Player player;
     private static Screen[] screens = new Screen[4];
 
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException {
         screens[0] = new StartMenu();
         for (int i = 1; i < screens.length; i++) {
             screens[i] = new Stage(ScreenType.STAGE);
@@ -20,7 +20,7 @@ public class Game {
     }
 
 
-    public static void init() throws InterruptedException {
+    public static void init() {
 
         player.setCurrentScreen(ScreenType.STAGE);
         player.setStage((Stage) screens[1]);
@@ -30,9 +30,10 @@ public class Game {
 
     public static void startStage(int stageNumber) {
         StartMenu menu = (StartMenu) screens[0];
-        System.out.println(menu.toString());
         menu.delete();
         player.setCurrentScreen(ScreenType.STAGE);
-        //screens[stageNumber].start();
+
+        screens[stageNumber].start();
+
     }
 }
